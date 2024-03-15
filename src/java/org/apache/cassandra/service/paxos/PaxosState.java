f8541b9ae8a149d1d963e7256d4aeb96333239e2
Jakub Zytka <jakub.zytka@datastax.com>
2024-03-15 16:14:43 +0100

    PaxosState::LOCKS stripe selection on basis of partition key and keyspace

--- src/java/org/apache/cassandra/service/paxos/PaxosState.java
+++ src/java/org/apache/cassandra/service/paxos/PaxosState.java
@@ -31,6 +31,7 @@ import org.apache.cassandra.db.Keyspace;
 import org.apache.cassandra.db.Mutation;
 import org.apache.cassandra.db.SystemKeyspace;
 import org.apache.cassandra.db.WriteOptions;
+import org.apache.cassandra.db.partitions.PartitionUpdate;
 import org.apache.cassandra.nodes.Nodes;
 import org.apache.cassandra.schema.TableMetadata;
 import org.apache.cassandra.tracing.Tracing;
@@ -66,7 +67,7 @@ public class PaxosState
         long start = System.nanoTime();
         try
         {
-            Lock lock = LOCKS.get(toPrepare.update.partitionKey());
+            Lock lock = LOCKS.get(lockStripe(toPrepare.update));
             lock.lock();
             try
             {
@@ -107,7 +108,7 @@ public class PaxosState
         long start = System.nanoTime();
         try
         {
-            Lock lock = LOCKS.get(proposal.update.partitionKey());
+            Lock lock = LOCKS.get(lockStripe(proposal.update));
             lock.lock();
             try
             {
@@ -166,5 +167,10 @@ public class PaxosState
         {
             Keyspace.open(proposal.update.metadata().keyspace).getColumnFamilyStore(proposal.update.metadata().id).metric.casCommit.addNano(System.nanoTime() - start);
         }
+    }
+
+    private static int lockStripe(PartitionUpdate update)
+    {
+        return update.partitionKey().hashCode() ^ update.metadata().keyspace.hashCode();
     }
 }
